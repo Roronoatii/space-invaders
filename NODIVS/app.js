ee887f893d0cba44ae077b2438871285fe646086
SiERRA74 <aksierra74@gmail.com>
2023-01-03 16:09:30 +0100

    machin

--- NODIVS/app.js
+++ NODIVS/app.js
@@ -1,10 +1,18 @@
-
-
+for (let i = 0; i < 20; i++) {
+  for (let j = 0; j < 16; j++) {
+    const newgrill = document.createElement("div");
+      if(i < 3 && j < 15){
+        newgrill.classList.add("alien");
+      }
+      if (i == 19 && j == 10){
+        newgrill.classList.add("tireur");
+      }
+    grid.appendChild(newgrill);
+  }
+}
 
 window.addEventListener('keydown', (e) => {
     console.log(e);
-  
-  
   });
 
 document.onkeydown = Spacekey;
